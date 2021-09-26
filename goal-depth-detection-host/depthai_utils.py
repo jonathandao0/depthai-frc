9cf00bf2b2e0093efd51b996def07883c02486d0
Jonathan Dao <jonathandao0@gmail.com>
2021-09-25 22:32:29 -0700

    Created goal-depth-detection-host

--- goal-depth-detection-host/depthai_utils.py
+++ goal-depth-detection-host/depthai_utils.py
@@ -18,36 +18,54 @@ def create_pipeline(model_name):
 
     # Define sources and outputs
     camRgb = pipeline.createColorCamera()
-    detectionNetwork = pipeline.createYoloDetectionNetwork()
+    detectionNetwork = pipeline.createYoloSpatialDetectionNetwork()
     edgeDetectorRgb = pipeline.createEdgeDetector()
     edgeManip = pipeline.createImageManip()
+    monoLeft = pipeline.createMonoCamera()
+    monoRight = pipeline.createMonoCamera()
+    stereo = pipeline.createStereoDepth()
 
-    xoutRgb = pipeline.createXLinkOut()
-    rgbControl = pipeline.createXLinkIn()
-    xinRgb = pipeline.createXLinkIn()
+    # xoutRgb = pipeline.createXLinkOut()
+    # rgbControl = pipeline.createXLinkIn()
+    # xinRgb = pipeline.createXLinkIn()
     xoutNN = pipeline.createXLinkOut()
     xoutEdgeRgb = pipeline.createXLinkOut()
-    edgeNN = pipeline.createXLinkOut()
+    xoutEdge = pipeline.createXLinkOut()
     xinEdgeCfg = pipeline.createXLinkIn()
 
-    xoutRgb.setStreamName("rgb")
-    xinRgb.setStreamName("rgbCfg")
-    rgbControl.setStreamName('rgbControl')
+    # xoutRgb.setStreamName("rgb")
+    # xinRgb.setStreamName("rgbCfg")
+    # rgbControl.setStreamName('rgbControl')
     xoutNN.setStreamName("detections")
     xoutEdgeRgb.setStreamName("edgeRgb")
     xinEdgeCfg.setStreamName("edgeCfg")
-    edgeNN.setStreamName("edgeNN")
+    xoutEdge.setStreamName("edge")
 
     # Properties
     camRgb.setPreviewSize(NN_IMG_SIZE, NN_IMG_SIZE)
-    camRgb.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
+    # camRgb.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
     camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
     camRgb.setInterleaved(False)
     camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-    camRgb.setFps(60)
+    camRgb.setFps(30) # 60 FPS is possible, but NN can't keep up
 
     edgeDetectorRgb.setMaxOutputFrameSize(camRgb.getVideoWidth() * camRgb.getVideoHeight())
     edgeManip.initialConfig.setResize(NN_IMG_SIZE, NN_IMG_SIZE)
+
+    monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+    monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+    monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+    monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+    # Setting node configs
+    # stereo.setOutputDepth(out_depth)
+    # stereo.setOutputRectified(out_rectified)
+    stereo.setConfidenceThreshold(255)
+    stereo.setRectifyEdgeFillColor(0)  # Black, to better see the cutout
+    stereo.setMedianFilter(dai.StereoDepthProperties.MedianFilter.KERNEL_7x7)  # KERNEL_7x7 default
+    # stereo.setLeftRightCheck(lrcheck)
+    # stereo.setExtendedDisparity(extended)
+    # stereo.setSubpixel(subpixel)
 
     model_dir = Path(__file__).parent.parent / Path(f"resources/nn/") / model_name
     blob_path = model_dir / Path(model_name).with_suffix(f".blob")
@@ -70,16 +88,20 @@ def create_pipeline(model_name):
     # Linking
     camRgb.preview.link(detectionNetwork.input)
     # detectionNetwork.passthrough.link(xoutRgb.input)
-    camRgb.preview.link(xoutRgb.input)
-    rgbControl.out.link(camRgb.inputControl)
-    xinRgb.out.link(camRgb.inputConfig)
+    # camRgb.preview.link(xoutRgb.input)
+    # rgbControl.out.link(camRgb.inputControl)
+    # xinRgb.out.link(camRgb.inputConfig)
     detectionNetwork.out.link(xoutNN.input)
 
     camRgb.video.link(edgeDetectorRgb.inputImage)
     # edgeDetectorRgb.outputImage.link(xoutEdgeRgb.input)
     edgeDetectorRgb.outputImage.link(edgeManip.inputImage)
-    edgeManip.out.link(edgeNN.input)
+    edgeManip.out.link(xoutEdge.input)
     xinEdgeCfg.out.link(edgeDetectorRgb.inputConfig)
+
+    monoLeft.out.link(stereo.left)
+    monoRight.out.link(stereo.right)
+    stereo.depth.link(detectionNetwork.inputDepth)
 
     log.info("Pipeline created.")
 
@@ -88,35 +110,35 @@ def create_pipeline(model_name):
 
 def capture(device_info):
     with dai.Device(pipeline, device_info) as device:
-        previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+        # previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
         detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
         # edgeRgbQueue = device.getOutputQueue("edgeRgb", 8, False)
-        edgeNNQueue = device.getOutputQueue("edgeNN", 8, False)
+        edgeQueue = device.getOutputQueue("edge", 8, False)
         edgeCfgQueue = device.getInputQueue("edgeCfg")
 
-        controlQueue = device.getInputQueue('rgbControl')
-        configQueue = device.getInputQueue('rgbCfg')
+        # controlQueue = device.getInputQueue('rgbControl')
+        # configQueue = device.getInputQueue('rgbCfg')
 
         while True:
-            cfg = dai.CameraControl()
-            cfg.setAutoFocusMode(dai.CameraControl.AutoFocusMode.OFF)
-            cfg.setAutoWhiteBalanceMode(dai.CameraControl.AutoWhiteBalanceMode.OFF)
-            cfg.setAutoExposureLock(True)
-            cfg.setAutoExposureCompensation(-6)
-            configQueue.send(cfg)
+            # cfg = dai.CameraControl()
+            # cfg.setAutoFocusMode(dai.CameraControl.AutoFocusMode.OFF)
+            # cfg.setAutoWhiteBalanceMode(dai.CameraControl.AutoWhiteBalanceMode.OFF)
+            # cfg.setAutoExposureLock(True)
+            # cfg.setAutoExposureCompensation(-6)
+            # configQueue.send(cfg)
 
-            frame = previewQueue.get().getCvFrame()
+            # frame = previewQueue.get().getCvFrame()
             inDet = detectionNNQueue.tryGet()
             # edgeFrame = edgeRgbQueue.get().getFrame()
-            edgeFrame = edgeNNQueue.get().getFrame()
+            edgeFrame = edgeQueue.get().getFrame()
 
             detections = []
             if inDet is not None:
                 detections = inDet.detections
 
             bboxes = []
-            height = frame.shape[0]
-            width  = frame.shape[1]
+            height = edgeFrame.shape[0]
+            width = edgeFrame.shape[1]
             for detection in detections:
                 bboxes.append({
                     'id': uuid.uuid4(),
@@ -128,9 +150,12 @@ def capture(device_info):
                     'y_min': int(detection.ymin * height),
                     'y_mid': int((detection.ymax - detection.ymin) * height),
                     'y_max': int(detection.ymax * height),
+                    'depth_x': detection.spatialCoordinates.x / 1000,
+                    'depth_y': detection.spatialCoordinates.y / 1000,
+                    'depth_z': detection.spatialCoordinates.z / 1000,
                 })
 
-            yield frame, bboxes, edgeFrame
+            yield edgeFrame, bboxes
 
 
 def del_pipeline():
---
+++ goal-depth-detection-host/main.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+
+import argparse
+import cv2
+import depthai as dai
+import socket
+
+import depthai_utils
+import logging
+import target_detection
+
+from common.mjpeg_stream import MjpegStream
+from networktables.util import NetworkTables
+from common.utils import FPSHandler
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', dest='debug', action="store_true", default=False, help='Start in Debug Mode')
+args = parser.parse_args()
+
+log = logging.getLogger(__name__)
+
+
+class Main:
+
+    def __init__(self):
+        log.info("Connected Devices:")
+        for device in dai.Device.getAllAvailableDevices():
+            log.info(f"{device.getMxId()} {device.state}")
+
+        self.init_networktables()
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+
+        ip_address = s.getsockname()[0]
+        port = 4201
+
+        self.device_list = {"OAK-D_Goal": {
+            'name': "OAK-D_Goal",
+            'id': "14442C10218CCCD200",
+            'stream_address': "{}:{}".format(ip_address, port),
+            'nt_tab': NetworkTables.getTable("OAK-D_Goal")
+        }}
+
+        self.object_pipeline, self.labels = depthai_utils.create_pipeline("infiniteRecharge2021")
+
+        self.oak_d_stream = MjpegStream(IP_ADDRESS=ip_address, HTTP_PORT=port)
+        self.fps = FPSHandler()
+
+    def parse_goal_frame(self, edgeFrame, bboxes):
+        valid_labels = ['red_upper_power_port', 'blue_upper_power_port']
+
+        nt_tab = self.device_list['OAK-D_Goal']['nt_tab']
+
+        for bbox in bboxes:
+            target_label = self.labels[bbox['label']]
+            if target_label not in valid_labels:
+                continue
+
+            edgeFrame, target_x, target_y = target_detection.find_largest_contour(edgeFrame, bbox)
+
+            if target_x == -999 or target_y == -999:
+                log.error("Error: Could not find target contour")
+                continue
+
+            angle_offset = (target_x - (depthai_utils.NN_IMG_SIZE / 2.0)) * 68.7938003540039 / 1080
+
+            if abs(angle_offset) > 30:
+                log.info("Invalid angle offset. Setting it to 0")
+                nt_tab.putNumber("tv", 0)
+                angle_offset = 0
+            else:
+                log.info("Found target '{}'\tX Angle Offset: {}".format(target_label, angle_offset))
+                nt_tab.putNumber("tv", 1)
+
+            nt_tab.putString("target_label", target_label)
+            nt_tab.putNumber("tx", angle_offset)
+
+            cv2.rectangle(edgeFrame, (bbox['x_min'], bbox['y_min']), (bbox['x_max'], bbox['y_max']), (255, 255, 255), 2)
+
+            cv2.circle(edgeFrame, (int(round(target_x, 0)), int(round(target_y, 0))), radius=5, color=(128, 128, 128), thickness=-1)
+
+            bbox['target_x'] = target_x
+            bbox['target_y'] = target_y
+            bbox['angle_offset'] = angle_offset
+
+        self.oak_d_stream.sendFrame(edgeFrame)
+
+        return edgeFrame, bboxes
+
+    def init_networktables(self):
+        NetworkTables.startClientTeam(4201)
+
+        if not NetworkTables.isConnected():
+            log.info("Could not connect to team client. Trying other addresses...")
+            NetworkTables.startClient([
+                '10.42.1.2',
+                '127.0.0.1',
+                '10.0.0.2',
+                '192.168.100.108'
+            ])
+
+        if NetworkTables.isConnected():
+            log.info("NT Connected to {}".format(NetworkTables.getRemoteAddress()))
+            return True
+        else:
+            log.error("Could not connect to NetworkTables. Restarting server...")
+            return False
+
+    def run(self):
+        log.info("Setup complete, parsing frames...")
+
+        try:
+            found, device_info = dai.Device.getDeviceByMxId(self.device_list['OAK-D_Goal']['id'])
+            self.device_list['OAK-D_Goal']['nt_tab'].putBoolean("OAK-D Goal Status", found)
+
+            if found:
+                self.device_list['OAK-D_Goal']['nt_tab'].putString("OAK-D_Goal Stream", self.device_list['OAK-D_Goal']['stream_address'])
+                for frame, bboxes in depthai_utils.capture(device_info):
+                    self.parse_goal_frame(frame, bboxes)
+                    self.fps.next_iter()
+                    log.debug("FPS: {}".format(self.fps.fps()))
+
+        finally:
+            log.info("Exiting Program...")
+
+
+class MainDebug(Main):
+
+    def __init__(self):
+        super().__init__()
+
+    def parse_goal_frame(self, frame, bboxes):
+        edgeFrame, bboxes = super().parse_goal_frame(frame, bboxes)
+        valid_labels = ['red_upper_power_port', 'blue_upper_power_port']
+
+        for bbox in bboxes:
+            target_label = self.labels[bbox['label']]
+
+            if target_label not in valid_labels:
+                continue
+
+            target_x = bbox['target_x'] if bbox['target_x'] is not None else 0
+            angle_offset = bbox['angle_offset'] if bbox['angle_offset'] is not None else 0
+
+            cv2.putText(edgeFrame, "x: {}".format(round(target_x, 2)), (bbox['x_min'], bbox['y_min'] + 30),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255))
+            cv2.putText(edgeFrame, "y: {}".format(round(bbox['y_mid'], 2)), (bbox['x_min'], bbox['y_min'] + 50),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255))
+            cv2.putText(edgeFrame, "z: {}".format(round(bbox['depth_z'], 2)), (bbox['x_min'], bbox['y_min'] + 70),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255))
+            cv2.putText(edgeFrame, "angle: {}".format(round(angle_offset, 3)), (bbox['x_min'], bbox['y_min'] + 90),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255))
+            cv2.putText(edgeFrame, "conf: {}".format(round(bbox['confidence'], 2)), (bbox['x_min'], bbox['y_min'] + 110),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255))
+            cv2.putText(edgeFrame, "label: {}".format(self.labels[bbox['label']], 1), (bbox['x_min'], bbox['y_min'] + 130),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255))
+
+        cv2.imshow("OAK-D Edge", edgeFrame)
+
+        key = cv2.waitKey(1)
+
+        if key == ord("q"):
+            raise StopIteration()
+
+
+if __name__ == '__main__':
+    if args.debug:
+        MainDebug().run()
+    else:
+        Main().run()
