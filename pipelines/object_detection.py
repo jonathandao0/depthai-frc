5d2dbd528002b744f5506b9d5137dab7e9ce2b83
Jonathan Dao <jonathandao0@gmail.com>
2021-10-03 00:41:45 -0700

    Created normal object tracker

---
+++ pipelines/object_detection.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import depthai as dai
+import uuid
+
+from common.config import *
+from pathlib import Path
+
+log = logging.getLogger(__name__)
+
+
+def create_pipeline(model_name):
+    global pipeline
+    log.info("Creating DepthAI pipeline...")
+
+    pipeline = dai.Pipeline()
+    pipeline.setOpenVINOVersion(dai.OpenVINO.Version.VERSION_2021_3)
+
+    # Define sources and outputs
+    camRgb = pipeline.createColorCamera()
+    detectionNetwork = pipeline.createYoloDetectionNetwork()
+
+    xoutRgb = pipeline.createXLinkOut()
+    nnOut = pipeline.createXLinkOut()
+
+    xoutRgb.setStreamName("rgb")
+    nnOut.setStreamName("detections")
+
+    # Properties
+    camRgb.setPreviewSize(NN_IMG_SIZE, NN_IMG_SIZE)
+    camRgb.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
+    camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    camRgb.setInterleaved(False)
+    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+    camRgb.setFps(60)
+
+    model_dir = Path(__file__).parent.parent / Path(f"resources/nn/") / model_name
+    blob_path = model_dir / Path(model_name).with_suffix(f".blob")
+
+    config_path = model_dir / Path(model_name).with_suffix(f".json")
+    nn_config = NNConfig(config_path)
+    labels = nn_config.labels
+
+    detectionNetwork.setBlobPath(str(blob_path))
+    detectionNetwork.setConfidenceThreshold(nn_config.confidence)
+    # detectionNetwork.setConfidenceThreshold(0.5)
+    detectionNetwork.setNumClasses(nn_config.metadata["classes"])
+    detectionNetwork.setCoordinateSize(nn_config.metadata["coordinates"])
+    detectionNetwork.setAnchors(nn_config.metadata["anchors"])
+    detectionNetwork.setAnchorMasks(nn_config.metadata["anchor_masks"])
+    detectionNetwork.setIouThreshold(nn_config.metadata["iou_threshold"])
+    detectionNetwork.setNumInferenceThreads(2)
+    detectionNetwork.input.setBlocking(False)
+
+    objectTracker.setDetectionLabelsToTrack([3])  # track only power cells
+    # possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS
+    objectTracker.setTrackerType(dai.TrackerType.ZERO_TERM_COLOR_HISTOGRAM)
+    # take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
+    objectTracker.setTrackerIdAssigmentPolicy(dai.TrackerIdAssigmentPolicy.UNIQUE_ID)
+
+    # Linking
+    camRgb.preview.link(detectionNetwork.input)
+    camRgb.preview.link(xoutRgb.input)
+    detectionNetwork.out.link(nnOut.input)
+
+    log.info("Pipeline created.")
+
+    return pipeline, labels
+
+
+def capture(device_info):
+    with dai.Device(pipeline, device_info) as device:
+        previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+        detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
+
+        while True:
+            frame = previewQueue.get().getCvFrame()
+            inDet = detectionNNQueue.tryGet()
+
+            detections = []
+            if inDet is not None:
+                detections = inDet.detections
+
+            bboxes = []
+            height = edgeFrame.shape[0]
+            width  = edgeFrame.shape[1]
+            for detection in detections:
+                bboxes.append({
+                    'id': uuid.uuid4(),
+                    'label': detection.label,
+                    'confidence': detection.confidence,
+                    'x_min': int(detection.xmin * width),
+                    'x_mid': int((detection.xmax - detection.xmin) * width),
+                    'x_max': int(detection.xmax * width),
+                    'y_min': int(detection.ymin * height),
+                    'y_mid': int((detection.ymax - detection.ymin) * height),
+                    'y_max': int(detection.ymax * height),
+                })
+
+            yield frame, bboxes
+
+
+def del_pipeline():
+    del pipeline
